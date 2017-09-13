cc9d55bcc6ab9f41dda47cc3fe6053aeecc7aa38
purnachander U <uppulapoorna@gmail.com>
2017-09-13 04:56:54 -0700

    int a=20

--- oms.java
+++ oms.java
@@ -1,8 +1,7 @@
 public class test
 {
-int a=10;
-int b=20;
-int c=30;
-int mul=a*b*c
-print ("multiplication  is")
+ int a=10:
+ int b=20;
+ int mul=a*b;
+  print ("multiplication of two numbers are")
 }
